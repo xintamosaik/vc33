0740303cde17395b610c7773e85d4a179044ee97
= <=>
2026-09-09 14:53:01 +0200

    initial commit

---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+ 
+	 
+	"net/http"
+)
+
+ 
+
+func main() {
+
+	http.HandleFunc("/", handleHome)
+ 
+	 
+	http.HandleFunc("GET /username", handleUsernameEdit)
+	http.HandleFunc("POST /username", handleRequestUpdateUserName)
+
+	http.ListenAndServe(":8080", nil)
+
+	fmt.Println("Server is running on port 8080")
+}
