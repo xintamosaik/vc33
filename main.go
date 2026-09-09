7f4a61f24374ebae3b4b9ecdb064a7e6ea4a2d0b
= <=>
2026-09-09 20:17:26 +0200

    small improvement

--- main.go
+++ main.go
@@ -1,12 +1,11 @@
 package main
 
 import (
-	"fmt"
+	"log"
 	 
 	"net/http"
 )
 
 func main() {
-	http.ListenAndServe(":8080", nil)
-	fmt.Println("Server is running on port 8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
