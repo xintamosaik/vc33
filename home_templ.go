8958d4ebb377165d7f0bbc092818362fb44d3eb5
= <=>
2026-09-10 13:23:21 +0200

    submit WIP

--- home_templ.go
+++ home_templ.go
@@ -55,7 +55,7 @@ func home() templ.Component {
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
-		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 2, "<p>address</p><p>jobs</p><p>education</p><p>skills</p><p>projects</p></main>")
+		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 2, "<p>jobs</p><p>education</p><p>skills</p><p>projects</p></main>")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
